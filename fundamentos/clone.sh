70e2d0d3340eb45808c6ce776cf9bd39aa137917
Marta <martacasgar@gmail.com>
2021-10-15 16:51:22 +0200

    hola.txt

---
+++ fundamentos/clone.sh
@@ -0,0 +1,6 @@
+
+git clone https://github.com/Martacsg/Edem2021MDA.git
+touch hola.txt
+git add *
+git commit -m "hola.txt"
+git push
---
+++ fundamentos/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir ejercicio0
+chmod 777 ejercicio0
+touch ejercicio0/README.md
+echo "BIENVENIDO A MI EJERCICIO" > ejercicio0/README.md
+
+sleep 10
+
+
+
